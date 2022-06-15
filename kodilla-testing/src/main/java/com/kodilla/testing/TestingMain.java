17b67916c68334f1477cde45c27da052dcdb35c3
EurekaII <rashlerr@gmail.com>
2022-06-15 10:01:32 +0200

    6.2 test class Calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,11 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        Calculator calculator = new Calculator();
+        calculator.add(5,6);
+        calculator.add(5,4);
     }
 }
