ef76a9885ec0aeb091bc5e02ff878ecd74d30976
EurekaII <rashlerr@gmail.com>
2022-06-22 05:42:39 +0200

    7.3 wykonanie zadania spacer po liście użytkowników forum

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/forumuser/ForumUser.java
@@ -0,0 +1,58 @@
+package com.kodilla.stream.forumuser;
+
+import java.time.LocalDate;
+
+public class ForumUser {
+    private static int count;
+
+    private int userId = count++;
+    private String userName;
+    private char gender;
+    private LocalDate birthdate;
+    private int posts;
+
+    public ForumUser(String userName, char gender, LocalDate birthdate, int posts) {
+        this.userName = userName;
+        this.gender = gender;
+        this.birthdate = birthdate;
+        this.posts = posts;
+    }
+    //for male
+    public boolean isMale() {
+        return gender == 'M' || gender == 'm';
+    }
+    //for female
+    public boolean isFemale() {
+        return gender == 'F' || gender == 'f';
+    }
+    public boolean isOver20() {
+        return LocalDate.now().getYear() - birthdate.getYear() >= 20;
+    }
+    public boolean hasPosts() {
+        return posts > 0;
+    }
+    public int getUserId() {
+        return userId;
+    }
+    public String getUserName() {
+        return userName;
+    }
+    public char getGender() {
+        return gender;
+    }
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+    public int getPosts() {
+        return posts;
+    }
+
+    @Override
+    public String toString() {
+        return "ForumUser{" +
+                "userId=" + userId +
+                ", userName='" + userName + '\'' +
+                ",birthdate=" + birthdate +
+                '}';
+    }
+}
